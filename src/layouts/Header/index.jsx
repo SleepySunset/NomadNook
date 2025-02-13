2fc46fa7f8ddba256e6dd4ea482030a6ec8fd1ae
saibeneseba <saibeneseba@gmail.com>
2025-02-12 22:31:28 -0300

    Creacion Header

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styles from "../Header/Header.module.css";
 
 const Header = () => {
   return (
     <header>
-        <Link to="/"><img src="" alt="Nomad Nook logo"></img></Link>
-        <div>
-            <button>Iniciar sesión</button>
-            <button>Crear cuenta</button>
-        </div>
+      <nav>
+        <ul>
+        <img src="/white_full_logo.png" alt="White-logo" className={styles.whitelogo} />
+         <h3>Viajar es vivir, hospedarte es sentir</h3>
+        </ul>
+      </nav>
+      <div className={styles.authLinksHeader}>
+        <Link to="/Register">Crear Cuenta</Link>
+        <Link to="/Login">Iniciar Sesión</Link>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
