0dc1e26cf715f5eee829cfc4fe3aed7ad67b828a
saibeneseba <saibeneseba@gmail.com>
2025-02-14 22:30:05 -0300

    mod 1 Header Footer y Home

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -1,15 +1,38 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
-    <footer>
-        <div>
-            <ul>
-                <li><a href="#"><img src="" alt="Instagram Logo"></img></a></li>
-                <li><a href="#"><img src="" alt="Facebook Logo"></img></a></li>
-                <li><a href="#"><img src="" alt="WhatsApp Logo"></img></a></li>
-            </ul>
-        </div>
-    </footer>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={styles.logoContainer}>
+        <img src="/white_full_logo.png" alt="White-logo" className={styles.whitelogo} />
+        <p className={styles.text}>
+          © {new Date().getFullYear()} Nomad Nook. Todos los derechos reservados.
+        </p>
+      </div>
 
-export default Footer
+      {/* Nueva fila solo para los iconos */}
+      <div className={styles.socialContainer}>
+        <ul className={styles.socialList}>
+          <li>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <img src="/instagram.png" alt="Instagram Logo" className={styles.socialIcon} />
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <img src="/facebook.png" alt="Facebook Logo" className={styles.socialIcon} />
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <img src="/whatsapp.png" alt="WhatsApp Logo" className={styles.socialIcon} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
