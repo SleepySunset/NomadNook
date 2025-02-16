3b24b1327099419b1572bad203b5f2b9bf7576f6
saibeneseba <saibeneseba@gmail.com>
2025-02-15 21:09:33 -0300

    Arreglos en Header Footer y Home 2

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -3,19 +3,20 @@ import styles from "../Header/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-        <img src="/white_full_logo.png" alt="White-logo" className={styles.whitelogo} />
-         <h3>Viajar es vivir, hospedarte es sentir</h3>
-        </ul>
-      </nav>
-      <div className={styles.authLinksHeader}>
-        <Link to="/Register">Crear Cuenta</Link>
-        <Link to="/Login">Iniciar Sesión</Link>
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Link to="/" className={styles.logoLink}>
+          <img src="/white_full_logo.png" alt="White-logo" className={styles.whitelogo} />
+          <h3 className={styles.slogan}>Viajar es vivir, hospedarte es sentir</h3>
+        </Link>
       </div>
+      
+      <nav className={styles.authLinksHeader}>
+        <Link to="/Register" className={styles.authButton}>Crear Cuenta</Link>
+        <Link to="/Login" className={styles.authButton}>Iniciar Sesión</Link>
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
