63b7b3a5c782f3556022a4c447f3abd457d02d91
juanbaquiroga <juanbaquiroga@gmail.com>
2025-02-18 21:46:17 -0300

    feat: add gallery to cabinDetail

--- src/pages/CabinDetail/index.jsx
+++ src/pages/CabinDetail/index.jsx
@@ -1,10 +1,23 @@
 import styles from "./CabinDetail.module.css";
 import { useParams } from "react-router-dom";
-import response from "../../utils/response";
+import { useState } from "react";
+import response from "@/utils/response";
+import {Link} from "react-router-dom";
+import Gallery from "@/components/Gallery";
+
 
 const CabinDetail = () => {
   const { id } = useParams();
   const cabin = response[id - 1];
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  }
 
   if (!cabin) {
     return <p className={styles.error}>Cabaña no encontrada.</p>;
@@ -12,24 +25,32 @@ const CabinDetail = () => {
 
   return (
     <main className={styles.detail}>
-      <h1 className={styles.title}>{cabin.titulo}</h1>
-      {cabin.imagenes && cabin.imagenes.length > 0 ? (
-        <div className={styles.imageContainer}>
-          <img className={styles.image} src={cabin.imagenes[0]} alt={`Imagen 1 de ${cabin.title}`}/>
-          <div className={styles.imageGallery}>
-            {cabin.imagenes.slice(1,5).map((image, index) => (
-              <img key={index} src={image} alt={`Imagen ${index + 2} de ${cabin.title}`} />
-            ))}
+      <div className={styles.container}>
+      <Link to="/" className={styles.backButton}>volver</Link>
+        <h1 className={styles.title}>{cabin.titulo}</h1>
+        {cabin.imagenes && cabin.imagenes.length > 0 ? (
+          <div className={styles.imagesContainer}>
+            <img className={styles.mainImage} onClick={handleShowModal} src={cabin.imagenes[0]} alt={`Imagen 1 de ${cabin.title}`}/>
+            <div className={styles.gallery}>
+              {cabin.imagenes.slice(1,5).map((image, index) => (
+                <img className={styles.galleryImg} onClick={handleShowModal} key={index} src={image} alt={`Imagen ${index + 2} de ${cabin.title}`} />
+              ))}
+            </div>
+            <button onClick={handleShowModal} className={styles.showAllImagesBtn}>
+              <p className={styles.allImagesText}>Mostrar todas las fotos</p>
+            </button>
           </div>
+        ) : (
+          <p>No hay imágenes disponibles.</p>
+        )}
+        <div className={styles.text}>
+          <span className={styles.price}>${cabin.precioPorNoche} por noche</span>
+          <p className={styles.description}>{cabin.descripcion}</p>
         </div>
-      ) : (
-        <p>No hay imágenes disponibles.</p>
-      )}
-      <button>Ver todas las fotos</button>
-      <div className={styles.text}>
-        <p className={styles.description}>{cabin.descripcion}</p>
-        <span className={styles.price}>${cabin.precioPorNoche} por noche</span>
       </div>
+      {showModal && (
+        <Gallery images={cabin.imagenes} onClose={handleCloseModal} />
+      )}
     </main>
   );
 };
