304cacfae9f2b624eff29f743293885fcd3f192d
rodriBRU1 <dbrunelrodri@gmail.com>
2025-02-20 22:58:18 -0500

    paginacion agregada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,38 @@
+
+import React, { useState } from "react";
 import styles from "./Home.module.css";
 import response from "../../utils/response";
 import Card from "../../components/Card";
-import Categories from "../../components/Categories"
+import Categories from "../../components/Categories";
 import Searchbar from "../../components/Searchbar";
+import Pagination from "../../components/Pagination"; 
 
 const Home = () => {
-  const randomCabins = response
-  ? [...response].sort(() => Math.random() - 0.5).slice(0, 10)
-  : [];
+  
+  const [paginaActual, setPaginaActual] = useState(1);
+  const cabinasPorPagina = 10; 
+
+  const cabinas = response ? [...response].sort(() => Math.random() - 0.5) : [];
+  const totalCabinas = cabinas.length;
+  const totalPaginas = Math.ceil(totalCabinas / cabinasPorPagina);
+
+  const inicio = (paginaActual - 1) * cabinasPorPagina;
+  const fin = inicio + cabinasPorPagina;
+  const cabinasPagina = cabinas.slice(inicio, fin);
+
+  const cambiarPagina = (nuevaPagina) => {
+    if (nuevaPagina >= 1 && nuevaPagina <= totalPaginas) {
+      setPaginaActual(nuevaPagina);
+    }
+  };
 
   return (
     <main className={styles.home}>
-      
-      <Categories/>
-      <Searchbar/>
-      
+      <Categories />
+      <Searchbar />
+
       <div className={styles.cabinGallery}>
-        {randomCabins.map((cabin) => (
+        {cabinasPagina.map((cabin) => (
           <Card
             key={cabin.id}
             id={cabin.id}
@@ -27,7 +43,12 @@ const Home = () => {
           />
         ))}
       </div>
-      
+
+      <Pagination
+        paginaActual={paginaActual}
+        totalPaginas={totalPaginas}
+        cambiarPagina={cambiarPagina}
+      />
     </main>
   );
 };
