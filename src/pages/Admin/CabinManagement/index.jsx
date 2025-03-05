ea533b9d74fd37307cb155ee2e791ab2d78e56fc
Luisa García <luisa.garcia1331@gmail.com>
2025-03-05 12:35:06 -0500

    Corrigiendo autotorizacion de gestion de cabañas

--- src/pages/Admin/CabinManagement/index.jsx
+++ src/pages/Admin/CabinManagement/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-
+import { useAuth } from "../../../hooks/AuthContext";
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import styles from "./CabinManagement.module.css";
 import AdminNav from "../../../components/AdminNav";
 import Searchbar from "../../../components/Searchbar";
@@ -8,6 +10,7 @@ import AdminTable from "../../../components/AdminTable";
 
 const CabinManagement = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { user, loading } = useAuth();
   useEffect(() => {
     if (isModalOpen) {
       document.body.style.overflow = "hidden";
@@ -26,6 +29,30 @@ const CabinManagement = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (!user?.token) {
+    console.log("No hay usuario autenticado:", user);
+    return <Navigate to="/login" replace />;
+  }else{
+    console.log(user)
+  }
+
+  let decoded;
+  try {
+    decoded = jwtDecode(user.token);
+
+    if (decoded.role !== "ROLE_ADMIN") {
+      console.log("Acceso denegado. Rol actual:", decoded.role);
+      return <Navigate to="/not-authorized" replace />;
+    }
+  } catch (error) {
+    console.error("Error al decodificar el token:", error);
+    return <Navigate to="/login" replace />;
+  }
 
   return (
     <>
